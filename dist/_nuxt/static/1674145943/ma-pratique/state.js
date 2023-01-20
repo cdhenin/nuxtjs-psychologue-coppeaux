4f5260c5b3854ccb1f6e1acda34f550a8b57ee03
Claire <claire.dhenin@nebulae.co>
2023-01-20 10:41:38 +0100

    Update dist

--- dist/_nuxt/static/1674145943/ma-pratique/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxtjs-psychologue-coppeaux\u002F_nuxt\u002Fstatic\u002F1674145943",layout:"default",error:a,serverRendered:true,routePath:"\u002Fma-pratique",config:{_app:{basePath:"\u002Fnuxtjs-psychologue-coppeaux\u002F",assetsPath:"\u002Fnuxtjs-psychologue-coppeaux\u002F_nuxt\u002F",cdnURL:a}}}}(null));
