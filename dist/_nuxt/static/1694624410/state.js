0e7fc039f2998ec15a0834a2aee0e59e8db5b186
Claire Dhénin <25612600+cdhenin@users.noreply.github.com>
2023-09-13 19:01:36 +0200

    update dist

---
+++ dist/_nuxt/static/1694624410/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1694624410",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},_img:{"/_ipx/s_94x28/img/logo_doctolib.svg":"\u002F_nuxt\u002Fimage\u002Ffccd4e.svg"}}}(null,"\u002F"));
